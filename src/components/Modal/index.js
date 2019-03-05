7ca8a21cb87d4a6fbb0af50df77bee101df867d2
v_aibotu <v_aibotu@tencent.com>
2019-03-05 19:57:20 +0800

    优化modal组件

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,9 @@
 import Taro , { Component } from '@tarojs/taro';
 import { View, Text , Button} from '@tarojs/components';
 import { AtModal, AtModalHeader, AtModalContent, AtModalAction } from "taro-ui"
+import { connect } from '@tarojs/redux'
 // import './index.scss';
+@connect((modal) => ({...modal}))
 export default class Index extends Component {
 
 
@@ -12,9 +14,7 @@ export default class Index extends Component {
         isOpened:false,
         title:'',
         content:'',
-        closeOnClickOverlay:true,
-        cancelBtn:true,
-        confirmBtn:true
+        closeOnClickOverlay:true
     }
     
     componentWillMount () {}
@@ -24,47 +24,22 @@ export default class Index extends Component {
     handleColse(){
         this.props.handleColse && this.props.handleColse();
     }
-    handleCancel(){
-        this.props.handleCancel && this.props.handleCancel();
-    }
-    handleConfirm(){
-        // this.props.handleConfirm && this.props.handleConfirm();
-        this.props.handleConfirm()
-    }
-    setOpenStatus(status){
-        this.setState({
-            isOpenedStatus:status
-        })
-    }
     render() {
-        const { isOpened, closeOnClickOverlay ,title , content , cancelText , onText ,cancelBtn , confirmBtn ,isOpenedStatus2} = this.props;
-        let status = isOpened;
-        console.log(isOpened,1111,status)
+        const { modalName, closeOnClickOverlay ,title , content , cancelText , onText ,hcancelBtn , hconfirmBtn} = this.props;
         return (
-            <AtModal
-                isOpened = {status} 
-                title={title}
-                cancelText='取消'
-                confirmText='确认'
-                onCancel={ () => this.handleCancel()}
-                onConfirm={ () => this.props.handleOk() }
-                content={content}
-            />
-        )
-        // return (
-        //     <AtModal 
-        //         isOpened = {status} 
-        //         closeOnClickOverlay = {closeOnClickOverlay}
-        //         >
-        //         {title && <AtModalHeader>title</AtModalHeader>}
-        //         <AtModalContent>
-        //             {content || this.props.children}
-        //         </AtModalContent>
-        //         <AtModalAction> 
-        //             {/* { cancelBtn && <Button onClick={() => this.handleCancel()}>{cancelText || "取消"}</Button> } */}
-        //             <Button onClick={() => this.props.handleOk()}>{onText || "确定"}</Button>
-        //         </AtModalAction>
-        //     </AtModal>
-        // );
+            <AtModal 
+                isOpened = {this.props.modal.modalsName[modalName]} 
+                closeOnClickOverlay = {closeOnClickOverlay}
+                >
+                {title && <AtModalHeader>{title}</AtModalHeader>}
+                <AtModalContent>
+                    {content || this.props.children}
+                </AtModalContent>
+                <AtModalAction> 
+                    {!hcancelBtn && <Button onClick={() => this.props.handleCancel()}>{cancelText || "取消"}</Button> }
+                    {!hconfirmBtn &&<Button onClick={() => this.props.handleOk()}>{onText || "确定"}</Button>}
+                </AtModalAction>
+            </AtModal>
+        );
     }
 }
