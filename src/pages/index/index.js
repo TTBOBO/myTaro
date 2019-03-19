e23c4d481bcc13643a531017e63e38389f4dae53
v_aibotu <v_aibotu@tencent.com>
2019-03-19 10:55:14 +0800

    完善首页轮播图效果

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text,Map  ,CoverView } from '@tarojs/components'
+import { View, Button, Text,Map  ,CoverView ,Swiper ,SwiperItem ,Image  } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 import { List  ,CardItem,TabBar} from '~/components'
 import { AtActionSheet, AtActionSheetItem } from "taro-ui"
@@ -21,17 +21,6 @@ class Index extends Component {
 
     async componentDidMount () {
         // const data = await Taro.$ajaxGet('test');
-        setTimeout(() =>{
-            wx.showActionSheet({
-                itemList: ['A', 'B', 'C'],
-                success(res) {
-                  console.log(res.tapIndex)
-                },
-                fail(res) {
-                  console.log(res.errMsg)
-                }
-              })
-        },3000)
     }
 
     componentDidHide () { 
@@ -41,7 +30,15 @@ class Index extends Component {
         this.props.dispatch({type:state == 'add' ? "user/add" : "user/del"}) 
     }
     state = {
-        
+        imgUrls: [
+            'http://image.wufazhuce.com/FnLVOKa7hPmA4wd1rSIDgEFgFwFS',
+            'http://image.wufazhuce.com/Fjp6hwvSOZbIjVrXw0SsSV1LN-rJ',
+            'http://image.wufazhuce.com/FoCDKbpGIOedXx26owKQzHlMyO5y',
+            'http://image.wufazhuce.com/FlAg5B17YIhVv8hUX8b6CwJkl4O6',
+            'http://image.wufazhuce.com/FtcXHjeXKrShdeFa9zGb9TDqEsef'
+        ],
+        current:2,
+        pageYStart:null
     }
     onTap(e){
         console.log(e)
@@ -52,15 +49,39 @@ class Index extends Component {
             url: '../auth/index'
         })
     }
+    touchStart(item,index,e){
+        this.setState({
+            pageYStart:e.touches[0].pageY
+        })
+    }
+    touchEnd(item,index,e){
+        if((this.state.pageYStart - e.changedTouches[0].pageY) > 10){
+            console.log('向上滑动10px')
+        }
+    }
 
   render () {
     return (
       <View className='container'>
-        <View className="header-bar"></View>
-        <View class="title" style={{marginBottom:'200px'}}>保量低风险</View>
-        <Map  />
-        <CoverView class="test">1111</CoverView >
-        {/* <TabBar /> */}
+        {/* <View className="header-bar"></View> */}
+        <View className='upadte-text'>已更新项目120</View>
+        <Swiper 
+            className='swiper'
+            indicatorColor='#999'
+            indicatorActiveColor='#333'
+            circular
+            // indicatorDots
+            current={this.state.current}
+            previousMargin="50px"
+            nextMargin="50px"
+            onChange={(e) => {this.setState({current:e.detail.current})}}
+        >
+            {this.state.imgUrls.map((item,index) => {
+                return (<SwiperItem className="swiper-item">
+                        <Image onTouchStart={this.touchStart.bind(this,item,index)} onTouchEnd={this.touchEnd.bind(this,item,index)} src={`http://10.6.52.35:8083/banner.png`} className={`slide-image ${this.state.current == index ? 'active' : ''}`}/>
+                </SwiperItem>)
+            })}
+        </Swiper>
       </View>
     )
   }
